0e8764de0ea2a60b4b5d23d3958fa8af0a1c3587
bartek-sta <bartlomiej.stadnik77@gmail.com>
2023-03-12 00:35:50 +0100

    Rviz ploting

--- lab_5_main.py
+++ lab_5_main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+import rospy
 from bag_reader import BagReader
 from map import OccupancyMap
 
@@ -15,16 +16,17 @@ class Plotter(object):
         plt.show()
 
 if __name__ == '__main__':
+    rospy.init_node('lab_5_main')
     plotter = Plotter()
     bag_reader = BagReader()
     scan_msgs = bag_reader.get_messages("/scan")
     pose_msgs = bag_reader.get_messages("/RosAria/pose")
 
     occupancy_map = OccupancyMap()
-    
+
     for i in range(700):
         scan_msg = scan_msgs[i]
         pose2D_msg = occupancy_map.make_pose_2D(pose_msgs[i])
         occupancy_map.update_map(scan_msg, pose2D_msg)
+        occupancy_map.publish_map()
     plotter.plot_heatmap(occupancy_map.occupancy_map)
-    
--- map.py
+++ map.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
+import rospy
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, OccupancyGrid
 from geometry_msgs.msg import Pose2D
 from tf.transformations import euler_from_quaternion
 from robotParams import *
@@ -12,15 +13,20 @@ import math
 class OccupancyMap(object):
     def __init__(self):
         self.occupancy_map = np.ones((MAP_SIZE, MAP_SIZE)) / 2
-    
+        self.pub = rospy.Publisher('occupancy_grid', OccupancyGrid, queue_size=10, latch=True)
+        self.skip_inf = True # flag for inf scan values skiping mode
+
     def update_map(self, laser_scan: LaserScan, pose: Pose2D):
         ranger_pose = pose
         ranger_pose.x = math.cos(pose.theta) * LASER_SHIFT_METER + pose.x
-        ranger_pose.y = math.sin(pose.theta) * LASER_SHIFT_METER + pose.y 
+        ranger_pose.y = math.sin(pose.theta) * LASER_SHIFT_METER + pose.y
         angle = laser_scan.angle_min
         for ray_range in laser_scan.ranges:
             if np.isinf(ray_range):
-                ray_range = laser_scan.range_max+1
+                if self.skip_inf:
+                    ray_range = np.NaN
+                else:
+                    ray_range = laser_scan.range_max+1
             if not np.isnan(ray_range):
                 empty_tiles, obstacle_tiles = bresenham.laser_through_tiles(ray_range, angle, ranger_pose, laser_scan.range_max)
                 for tile in empty_tiles:
@@ -38,7 +44,7 @@ class OccupancyMap(object):
         y = odometry_msg.pose.pose.position.y
 
         return Pose2D(x, y, yaw)
-        
+
     def cell_probability_update(self, cell: bresenham.grid_pos_t, p: bool):
         if p is True:
             update = 0.1
@@ -50,6 +56,20 @@ class OccupancyMap(object):
         elif self.occupancy_map[cell.grid_y][cell.grid_x] > 1:
             self.occupancy_map[cell.grid_y][cell.grid_x] = 1
 
-        
+    def publish_map(self):
+        msg = OccupancyGrid()
+
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = 'map'
+
+        msg.info.width = self.occupancy_map.shape[1]
+        msg.info.height = self.occupancy_map.shape[0]
+        msg.info.resolution = CELL_SIZE_METER
+
+        data = np.array(self.occupancy_map.flatten() * 100, dtype=np.int8)
+        msg.data = list(data)
+        self.pub.publish(msg)
+
+
 if __name__ == '__main__':
     occupancy_map = OccupancyMap()

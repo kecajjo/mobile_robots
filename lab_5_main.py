2af2471e3f141df28d916664ed8f8d3d235e5b4d
bartek-sta <bartlomiej.stadnik77@gmail.com>
2023-03-13 17:42:32 +0100

    Read data from json or bag

--- lab_5_main.py
+++ lab_5_main.py
@@ -2,8 +2,11 @@
 
 import matplotlib.pyplot as plt
 import rospy
+import math
 from bag_reader import BagReader
 from map import OccupancyMap
+from laserSub import LaserScanListener
+from geometry_msgs.msg import Pose2D
 
 
 class Plotter(object):
@@ -15,18 +18,33 @@ class Plotter(object):
         plt.colorbar()
         plt.show()
 
-if __name__ == '__main__':
-    rospy.init_node('lab_5_main')
-    plotter = Plotter()
+def map_from_bag(msg_count):
     bag_reader = BagReader()
+    occupancy_map = OccupancyMap()
     scan_msgs = bag_reader.get_messages("/scan")
     pose_msgs = bag_reader.get_messages("/RosAria/pose")
 
-    occupancy_map = OccupancyMap()
-
-    for i in range(700):
+    for i in range(msg_count):
         scan_msg = scan_msgs[i]
         pose2D_msg = occupancy_map.make_pose_2D(pose_msgs[i])
         occupancy_map.update_map(scan_msg, pose2D_msg)
         occupancy_map.publish_map()
+    return occupancy_map
+
+def map_from_json(msg_count):
+    laser_scan = LaserScanListener()
+    occupancy_map = OccupancyMap()
+
+    for i in range(msg_count):
+        scan_msg, pose = laser_scan.load_json_scan(i)
+        pose2D_msg = Pose2D(pose[0], pose[1], math.radians(pose[2]))
+        occupancy_map.update_map(scan_msg, pose2D_msg)
+        occupancy_map.publish_map()
+    return occupancy_map
+
+if __name__ == '__main__':
+    rospy.init_node('lab_5_main')
+    plotter = Plotter()
+    # occupancy_map = map_from_json(9)
+    occupancy_map = map_from_bag(1500)
     plotter.plot_heatmap(occupancy_map.occupancy_map)
